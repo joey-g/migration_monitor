cbb8ab306c6a359668759f38086819398ce16d0d
joey-g <joey.gryder@gmail.com>
2024-03-28 22:23:35 -0400

    Add User model and initial table create migration

--- migration_monitor/settings.py
+++ migration_monitor/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'migration_monitor',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
