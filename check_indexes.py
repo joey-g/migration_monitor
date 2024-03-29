8c075966f96cdc1e25068522e00cf44ea3197ad3
joey-g <joey.gryder@gmail.com>
2024-03-28 21:53:33 -0400

    Stub out check_indexes workflow action

---
+++ check_indexes.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+def check_migration(filename):
+    with open(filename, 'r') as file:
+        contents = file.read()
+
+    # This regular expression matches 'CREATE INDEX' statements that are not followed by 'CONCURRENTLY'.
+    # It uses a negative lookahead assertion to ensure 'CONCURRENTLY' does not follow 'CREATE INDEX'.
+    if re.search(r'CREATE INDEX (?!CONCURRENTLY)', contents, re.MULTILINE | re.IGNORECASE):
+        print(f'Warning: {filename} adds an index without the concurrently option.')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    check_migration_file(sys.argv[1])
