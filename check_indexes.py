8787021474f99c4f91a2c95f1e45bee5dd9e1aae
joey-g <joey.gryder@gmail.com>
2024-04-23 22:46:23 -0400

    Continue to tweak regex to be more flexible

--- check_indexes.py
+++ check_indexes.py
@@ -7,12 +7,16 @@ def check_migration(contents):
     print('Migration contents:')
     print(contents)
     for table in TABLES_TO_TEST:
-        # This regular expression matches 'CREATE INDEX' statements for the given table
-        # that are not followed by 'CONCURRENTLY'.
-        # It uses a negative lookahead assertion to ensure 'CONCURRENTLY' does not follow 'CREATE INDEX'.
-        if re.search(r'CREATE INDEX .* ON {} (?!CONCURRENTLY)'.format(table), contents, re.MULTILINE | re.IGNORECASE):
+        # Regular expression matches 'CREATE INDEX' statements for the given table that are
+        # not followed by 'CONCURRENTLY'. oUses a negative lookahead assertion to ensure
+        # 'CONCURRENTLY' does not follow 'CREATE INDEX'.
+        regex = r'CREATE INDEX .* ON "?{}"? \S+'.format(table)
+        if re.search(regex, contents, re.MULTILINE | re.IGNORECASE):
             print('Warning: Migration adds an index without the CONCURRENTLY option on table {}'.format(table))
             sys.exit(1)
+        else:
+            print('Migration is valid')
+            sys.exit(0)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
