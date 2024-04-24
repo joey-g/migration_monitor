4fc0c25ea3541b38095732b959c4bc5e6bd07eb4
joey-g <joey.gryder@gmail.com>
2024-04-23 22:25:48 -0400

    Extend check_indexes and workflow to expand and test against raw SQL

--- check_indexes.py
+++ check_indexes.py
@@ -1,15 +1,23 @@
 import re
 import sys
 
-def check_migration(filename):
-    with open(filename, 'r') as file:
-        contents = file.read()
+TABLES_TO_TEST = ['migration_monitor_user']
 
-    # This regular expression matches 'CREATE INDEX' statements that are not followed by 'CONCURRENTLY'.
-    # It uses a negative lookahead assertion to ensure 'CONCURRENTLY' does not follow 'CREATE INDEX'.
-    if re.search(r'CREATE INDEX (?!CONCURRENTLY)', contents, re.MULTILINE | re.IGNORECASE):
-        print(f'Warning: {filename} adds an index without the concurrently option.')
-        sys.exit(1)
+def check_migration(contents):
+    for table in TABLES_TO_TEST:
+        # This regular expression matches 'CREATE INDEX' statements for the given table
+        # that are not followed by 'CONCURRENTLY'.
+        # It uses a negative lookahead assertion to ensure 'CONCURRENTLY' does not follow 'CREATE INDEX'.
+        if re.search(r'CREATE INDEX .* ON {} (?!CONCURRENTLY)'.format(table), contents, re.MULTILINE | re.IGNORECASE):
+            print('Warning: Migration adds an index without the CONCURRENTLY option on table {}'.format(table))
+            print('Migration contents:')
+            print(contents)
+            sys.exit(1)
 
 if __name__ == '__main__':
-    check_migration(sys.argv[1])
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'r') as file:
+            contents = file.read()
+        check_migration(contents)
+    else:
+        check_migration(sys.stdin.read())
--- migration_monitor/models.py
+++ migration_monitor/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+
 class User(models.Model):
     first_name = models.CharField(max_length=35)
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, null=True, blank=True
+    )
